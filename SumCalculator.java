0d79673a6786b64fb779a6ff68d6d7cbc1589976
khushisingh2005 <khushisingh2822005@gmail.com>
2024-10-17 20:27:21 +0530

    Update SumCalculator.java

--- SumCalculator.java
+++ SumCalculator.java
@@ -1,19 +1,9 @@
-import java.util.Scanner;
-
 public class SumCalculator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Enter first number: ");
-        int num1 = scanner.nextInt();
-        
-        System.out.print("Enter second number: ");
-        int num2 = scanner.nextInt();
+        int num1 = 5; // replace with test value
+        int num2 = 10; // replace with test value
         
         int sum = num1 + num2;
-        
         System.out.println("The sum is: " + sum);
-        
-        scanner.close();
     }
 }
